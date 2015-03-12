21320c0f2e3c4ffada4c6ee1522290049e47fbb4
Falco Nogatz <fnogatz@gmail.com>
2015-03-12 22:46:33 +0100

    Travis Build eingerichtet

---
+++ deploy-ghpages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Richte git ein"
+
+CURRENT_COMMIT=`git rev-parse HEAD`
+
+git clone -b gh-pages "https://${GH_TOKEN}@${GH_REF}" exporte > /dev/null 2>&1 || exit 1
+
+echo "Lade md-tools"
+git clone -b master "https://github.com/Schachjugend/md-tools.git" md-tools > /dev/null 2>&1 || exit 1
+
+rm -r exporte/*
+
+echo "Erstelle Exporte"
+./md-tools/bin/schachjugend-md spielordnung all ./Spielordnung.md ./exporte || exit 1
+
+cd exporte/
+
+echo "Push auf gh-pages"
+git config user.name "$GIT_NAME"
+git config user.email "$GIT_EMAIL"
+
+git add -A
+git commit --allow-empty -m "Aktuelle Fassung per $CURRENT_COMMIT" || exit 1
+git push origin gh-pages > /dev/null 2>&1 || exit 1
+
+echo "Exporte erfolgreich erstellt."
+exit 0
